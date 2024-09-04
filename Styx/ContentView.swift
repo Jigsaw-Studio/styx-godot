8695a45573e5d1947087ac14853d953519d5786a
Steve Castellotti <sc@jigsaw.studio>
2024-09-04 06:02:50 -0700

    Initial commit

---
+++ Styx/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Styx
+//
+//  Created by Steve Castellotti on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Styx/StyxApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyxApp.swift
+//  Styx
+//
+//  Created by Steve Castellotti on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct StyxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

2be9561ef3e1ec4df2bedab41ca032753f13e0df
Steve Castellotti <sc@jigsaw.studio>
2025-01-26 21:22:02 -0800

    Update support for WebXR web export

--- Godot_Project/bar_chart/styx_api.gd
+++ Godot_Project/bar_chart/styx_api.gd
@@ -2,18 +2,22 @@ extends Node
 
 class_name StyxApi
 
+var is_running_in_web : bool = OS.get_name() == "Web" or OS.get_name() == "HTML5"
+
 # Load the network visualization script
 var network_visualization = preload("res://bar_chart/network_visualization.gd")
 var nv = null
 
-# UDP client setup
+# Network client setup
 var udp_client: PacketPeerUDP = PacketPeerUDP.new()
+var tcp_client: WebSocketPeer = WebSocketPeer.new()
 var packet_fragments := {}
 var expected_fragments := {}
 
 # Define server settings
 var server_ip: String = "172.16.100.1"
-var server_port: int = 8192
+var server_port_udp: int = 8192
+var server_port_tcp: int = 443
 
 # Endpoints
 var endpoints: Dictionary = {
@@ -41,11 +45,19 @@ func init(endpoint: String) -> void:
     nv = network_visualization.new()
     add_child(nv)
 
-    # Create and bind the UDP client to any available port
-    var bind_result: int = udp_client.bind(0)
-    if bind_result != OK:
-        print("Failed to bind UDP client.")
-        return
+    if not is_running_in_web:
+        # Create and bind the UDP client to any available port
+        var bind_result: int = udp_client.bind(0)
+        if bind_result != OK:
+            print("Failed to bind UDP client.")
+    else:
+        var endpoint_data = endpoints.get(current_endpoint)
+        var url = "wss://%s%s" % [server_ip, endpoint_data.api_path]
+        var connect_result = tcp_client.connect_to_url(url, TLSOptions.client_unsafe())
+        if connect_result == OK:
+            print("Connected to WebSocket server:", url)
+        else:
+            print("Failed to connect to WebSocket server. Error:", connect_result)
 
 func send_request() -> void:
     var endpoint_data = endpoints.get(current_endpoint)
@@ -53,65 +65,116 @@ func send_request() -> void:
         print("Endpoint not found:", current_endpoint)
         return
 
-    var json_data: Dictionary = {"GET": endpoint_data.api_path}
-    var json_str: String = JSON.stringify(json_data)
+    if not is_running_in_web:
+        var json_data: Dictionary = {"GET": endpoint_data.api_path}
+        var json_str: String = JSON.stringify(json_data)
 
-    # Connect to the server's IP and port
-    udp_client.connect_to_host(server_ip, server_port)
-
-    # Convert to bytes and send the UDP packet
-    var packet: PackedByteArray = json_str.to_utf8_buffer()
-    var sent: int = udp_client.put_packet(packet)
-    if sent == OK:
-        print("Sent:", json_str)
+        # Send request via UDP
+        udp_client.connect_to_host(server_ip, server_port_udp)
+        var packet: PackedByteArray = json_str.to_utf8_buffer()
+        var sent: int = udp_client.put_packet(packet)
+        if sent == OK:
+            print("Sent via UDP:", json_str)
+        else:
+            print("Failed to send request via UDP.")
     else:
-        print("Failed to send request.")
+        if tcp_client.get_ready_state() == WebSocketPeer.STATE_CLOSED:
+            # Get API endpoint via secure TCP WebSocket
+            var url = "wss://%s%s" % [server_ip, endpoint_data.api_path]
+            var connect_result = tcp_client.connect_to_url(url, TLSOptions.client_unsafe())
+            if connect_result == OK:
+                print("Connected to WebSocket server:", url)
+                set_process(true)
+            else:
+                print("Failed to connect to WebSocket server. Error:", connect_result)
 
 func _process(_delta):
-    # Continuously check for incoming packets
-    receive_response()
+    if not is_running_in_web:
+        receive_udp_response()
+    else:
+        var state = tcp_client.get_ready_state()
+        if state == WebSocketPeer.STATE_OPEN:
+            receive_tcp_response()
+        elif state == WebSocketPeer.STATE_CLOSING:
+            pass
+        elif state == WebSocketPeer.STATE_CLOSED:
+            var code = tcp_client.get_close_code()
+            var reason = tcp_client.get_close_reason()
+            print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
+            set_process(false) # Stop processing.
+        else:
+            print("tcp_client.get_ready_state():", state)
 
-func receive_response():
-    while udp_client.get_available_packet_count() > 0:
-        var packet: PackedByteArray = udp_client.get_packet()
+func receive_tcp_response():
+    while tcp_client.get_available_packet_count():
+        var packet: PackedByteArray = tcp_client.get_packet()
+        print("packet: ", packet)
         var response: String = packet.get_string_from_utf8()
+        print("response: ", response)
+        process_response_tcp(response)
 
-        # Parse the fragment
-        var delimiter_index: int = response.find(":")
-        if delimiter_index == -1:
-            print("Received malformed packet: %s" % response)
-            continue
-
-        var header: String = response.substr(0, delimiter_index)
-        var body: String = response.substr(delimiter_index + 1)
-
-        # Parse the header (format: "fragment_num/total_fragments")
-        var fragment_info: PackedStringArray = header.split("/")
-        if fragment_info.size() != 2:
-            print("Received malformed packet header: %s" % header)
-            continue
-
-        var fragment_num: int = int(fragment_info[0])
-        var total_fragments: int = int(fragment_info[1])
-
-        # Store the fragment
-        if !packet_fragments.has(fragment_num):
-            packet_fragments[fragment_num] = body
-
-        # Check if all fragments have been received
-        if packet_fragments.size() == total_fragments:
-            var complete_response: String = ""
-            for i in range(1, total_fragments + 1):
-                complete_response += packet_fragments[i]
-
-            # All fragments received, process the response
-            _on_request_completed(null, 200, [], complete_response)
-
-            # Clear stored fragments for the next message
-            packet_fragments.clear()
+func process_response_tcp(response: String):
+    _on_request_completed_tcp(null, 200, [], response)
 
 # Function to handle the completion of the HTTP request
-func _on_request_completed(_result, response_code, _headers, body):
+func _on_request_completed_tcp(_result, response_code, _headers, body):
+    if response_code == 200:
+        print("Processing response:", body)
+        var endpoint_data = endpoints.get(current_endpoint)
+        if endpoint_data != null:
+            var calculate_method = endpoint_data.calculate
+            var visualize_method = endpoint_data.visualize
+
+            var data: Dictionary = call(calculate_method, body)
+            call(visualize_method, data)
+        else:
+            print("Endpoint configuration not found for", current_endpoint)
+    else:
+        print("Request failed with response code:", response_code)
+
+func receive_udp_response():
+    while udp_client.get_available_packet_count():
+        var packet: PackedByteArray = udp_client.get_packet()
+        var response: String = packet.get_string_from_utf8()
+        process_response_udp(response)
+
+func process_response_udp(response: String):
+    # Parse the fragment
+    var delimiter_index: int = response.find(":")
+    if delimiter_index == -1:
+        print("Received malformed packet: %s" % response)
+        return
+
+    var header: String = response.substr(0, delimiter_index)
+    var body: String = response.substr(delimiter_index + 1)
+
+    # Parse the header (format: "fragment_num/total_fragments")
+    var fragment_info: PackedStringArray = header.split("/")
+    if fragment_info.size() != 2:
+        print("Received malformed packet header: %s" % header)
+        return
+
+    var fragment_num: int = int(fragment_info[0])
+    var total_fragments: int = int(fragment_info[1])
+
+    # Store the fragment
+    if !packet_fragments.has(fragment_num):
+        packet_fragments[fragment_num] = body
+
+    # Check if all fragments have been received
+    if packet_fragments.size() == total_fragments:
+        var complete_response: String = ""
+        for i in range(1, total_fragments + 1):
+            complete_response += packet_fragments[i]
+
+        # All fragments received, process the response
+        _on_request_completed_udp(null, 200, [], complete_response)
+
+        # Clear stored fragments for the next message
+        packet_fragments.clear()
+
+# Function to handle the completion of the HTTP request
+func _on_request_completed_udp(_result, response_code, _headers, body):
     if response_code == 200:
         var json = JSON.new()
         var json_result = json.parse(body)
@@ -135,6 +198,7 @@ func _on_request_completed(_result, response_code, _headers, body):
             print("Failed to parse JSON: ", json.error_string)
     else:
         print("Request failed with response code: ", response_code)
+
 
 func calculate_traffic_remote(parsed_data: Array) -> Dictionary:
     return nv.calculate_traffic_remote(parsed_data)
--- Godot_Project/main.gd
+++ Godot_Project/main.gd
@@ -2,21 +2,47 @@ extends Node3D
 
 var xr_interface : XRInterface
 var passthrough_enabled : bool = false
+var is_running_in_web : bool = OS.get_name() == "Web" or OS.get_name() == "HTML5"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    xr_interface = XRServer.find_interface("OpenXR")
-    if xr_interface and xr_interface.is_initialized():
-        print("OpenXR initialized")
 
-        # Turn off v-sync!
-        DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
+    if not is_running_in_web:
+        $WebCanvasLayer.visible = false
+        
+        xr_interface = XRServer.find_interface("OpenXR")
+        if xr_interface and xr_interface.is_initialized():
+            print("OpenXR initialized")
+            
+            # Turn off v-sync!
+            DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 
-        get_viewport().use_xr = true
+            get_viewport().use_xr = true
 
-        passthrough_enabled = enable_passthrough()
-    else:
-        print("OpenXR not initialized, please check if your headset is connected")
+            passthrough_enabled = enable_passthrough()
+        else:
+            print("OpenXR not initialized, please check if your headset is connected")
+
+    else:        
+        $WebCanvasLayer.visible = true
+    
+        xr_interface = XRServer.find_interface("WebXR")
+        if xr_interface:
+            print("WebXR initialized")
+            
+            $WebCanvasLayer/WebButton.pressed.connect(self._on_button_pressed)
+            # WebXR uses a lot of asynchronous callbacks, so we connect to various
+            # signals in order to receive them.
+            xr_interface.session_supported.connect(self._webxr_session_supported)
+            xr_interface.session_started.connect(self._webxr_session_started)
+            xr_interface.session_ended.connect(self._webxr_session_ended)
+            xr_interface.session_failed.connect(self._webxr_session_failed)
+    
+            # This returns immediately - our _webxr_session_supported() method
+            # (which we connected to the "session_supported" signal above) will
+            # be called sometime later to let us know if it's supported or not.
+            #xr_interface.is_session_supported("immersive-vr")
+            xr_interface.is_session_supported("immersive-ar")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
@@ -37,3 +63,61 @@ func enable_passthrough() -> bool:
         get_viewport().transparent_bg = true
 
     return true
+
+func _webxr_session_supported(session_mode: String, supported: bool) -> void:
+    if session_mode == 'immersive-vr':
+        if supported:
+            $WebCanvasLayer.visible = true
+        else:
+            OS.alert("Your browser doesn't support VR")
+    elif session_mode == 'immersive-ar':
+        if supported:
+            $WebCanvasLayer.visible = true
+        else:
+            OS.alert("Your browser doesn't support AR")
+ 
+func _on_button_pressed() -> void:
+    # Whether we want an immersive VR session, as opposed to AR ('immersive-ar') or a
+    # simple 3DoF viewer ('viewer').
+    #xr_interface.session_mode = 'immersive-vr'
+    xr_interface.session_mode = 'immersive-ar'
+    # 'bounded-floor' is room scale, 'local-floor' is a standing or sitting
+    # experience (it puts you 1.6m above the ground if you have 3DoF headset),
+    # whereas as 'local' puts you down at the ARVROrigin.
+    # This list means it'll first try to request 'bounded-floor', then
+    # fallback on 'local-floor' and ultimately 'local', if nothing else is
+    # supported.
+    xr_interface.requested_reference_space_types = 'bounded-floor, local-floor, local'
+    ## In order to use 'local-floor' or 'bounded-floor' we must also
+    ## mark the features as required or optional.
+    xr_interface.required_features = 'local-floor'
+    xr_interface.optional_features = 'bounded-floor'
+ 
+    # This will return false if we're unable to even request the session,
+    # however, it can still fail asynchronously later in the process, so we
+    # only know if it's really succeeded or failed when our
+    # _webxr_session_started() or _webxr_session_failed() methods are called.
+    if not xr_interface.initialize():
+        OS.alert("Failed to initialize WebXR")
+        return
+ 
+func _webxr_session_started() -> void:
+    $WebCanvasLayer.visible = false
+    # This tells Godot to start rendering to the headset.
+    get_viewport().use_xr = true
+    
+    passthrough_enabled = enable_passthrough()
+
+    # This will be the reference space type you ultimately got, out of the
+    # types that you requested above. This is useful if you want the game to
+    # work a little differently in 'bounded-floor' versus 'local-floor'.
+    print ("Reference space type: " + xr_interface.reference_space_type)
+ 
+func _webxr_session_ended() -> void:
+    $WebCanvasLayer.visible = true
+    # If the user exits immersive mode, then we tell Godot to render to the web
+    # page again.
+    get_viewport().use_xr = false
+ 
+func _webxr_session_failed(message: String) -> void:
+    OS.alert("Failed to initialize: " + message)

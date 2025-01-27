da17f7618562ab86647b9c4c008b913a21299085
Steve Castellotti <sc@jigsaw.studio>
2025-01-27 00:11:57 -0800

    Added HTTPS request support for visualization data when using WebXR web export

--- Godot_Project/bar_chart/styx_api.gd
+++ Godot_Project/bar_chart/styx_api.gd
@@ -8,16 +8,16 @@ var is_running_in_web : bool = OS.get_name() == "Web" or OS.get_name() == "HTML5
 var network_visualization = preload("res://bar_chart/network_visualization.gd")
 var nv = null
 
-# Network client setup
+# Network client setup (UDP for native clients, TCP HTTPRequest for WebXR web export)
+var http_client: HTTPRequest = HTTPRequest.new()
 var udp_client: PacketPeerUDP = PacketPeerUDP.new()
-var tcp_client: WebSocketPeer = WebSocketPeer.new()
 var packet_fragments := {}
 var expected_fragments := {}
 
 # Define server settings
 var server_ip: String = "172.16.100.1"
-var server_port_udp: int = 8192
 var server_port_tcp: int = 443
+var server_port_udp: int = 8192
 
 # Endpoints
 var endpoints: Dictionary = {
@@ -45,19 +45,19 @@ func init(endpoint: String) -> void:
     nv = network_visualization.new()
     add_child(nv)
 
+# Ensure the node is fully initialized and added to the scene tree before making the network requests
+func _ready():
     if not is_running_in_web:
         # Create and bind the UDP client to any available port
         var bind_result: int = udp_client.bind(0)
         if bind_result != OK:
-            print("Failed to bind UDP client.")
+            print("Failed to bind UDP client")
     else:
-        var endpoint_data = endpoints.get(current_endpoint)
-        var url = "wss://%s%s" % [server_ip, endpoint_data.api_path]
-        var connect_result = tcp_client.connect_to_url(url, TLSOptions.client_unsafe())
-        if connect_result == OK:
-            print("Connected to WebSocket server:", url)
-        else:
-            print("Failed to connect to WebSocket server. Error:", connect_result)
+        # Add HTTPRequest to the scene tree
+        add_child(http_client)
+        # Connect the request_completed signal
+        http_client.connect("request_completed", self._process_response_http)
+        send_request()
 
 func send_request() -> void:
     var endpoint_data = endpoints.get(current_endpoint)
@@ -74,63 +74,23 @@ func send_request() -> void:
         var packet: PackedByteArray = json_str.to_utf8_buffer()
         var sent: int = udp_client.put_packet(packet)
         if sent == OK:
-            print("Sent via UDP:", json_str)
+            print("Sent via UDP: ", json_str)
         else:
-            print("Failed to send request via UDP.")
+            print("Failed to send request via UDP")
     else:
-        if tcp_client.get_ready_state() == WebSocketPeer.STATE_CLOSED:
-            # Get API endpoint via secure TCP WebSocket
-            var url = "wss://%s%s" % [server_ip, endpoint_data.api_path]
-            var connect_result = tcp_client.connect_to_url(url, TLSOptions.client_unsafe())
-            if connect_result == OK:
-                print("Connected to WebSocket server:", url)
-                set_process(true)
-            else:
-                print("Failed to connect to WebSocket server. Error:", connect_result)
+        var status = http_client.get_http_client_status()
+        if status == HTTPClient.STATUS_DISCONNECTED:
+            var url = "https://%s:%d%s" % [server_ip, server_port_tcp, endpoint_data.api_path]
+            print("Connecting to URL: ", url)
+            var error = http_client.request(url, [], HTTPClient.METHOD_GET)
+            if error != OK:
+                print("Failed to initiate HTTP request. Error: ", error)
+        else:
+            print("Delaying HTTP request, status: ", status)
 
 func _process(_delta):
     if not is_running_in_web:
         receive_udp_response()
-    else:
-        var state = tcp_client.get_ready_state()
-        if state == WebSocketPeer.STATE_OPEN:
-            receive_tcp_response()
-        elif state == WebSocketPeer.STATE_CLOSING:
-            pass
-        elif state == WebSocketPeer.STATE_CLOSED:
-            var code = tcp_client.get_close_code()
-            var reason = tcp_client.get_close_reason()
-            print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
-            set_process(false) # Stop processing.
-        else:
-            print("tcp_client.get_ready_state():", state)
-
-func receive_tcp_response():
-    while tcp_client.get_available_packet_count():
-        var packet: PackedByteArray = tcp_client.get_packet()
-        print("packet: ", packet)
-        var response: String = packet.get_string_from_utf8()
-        print("response: ", response)
-        process_response_tcp(response)
-
-func process_response_tcp(response: String):
-    _on_request_completed_tcp(null, 200, [], response)
-
-# Function to handle the completion of the HTTP request
-func _on_request_completed_tcp(_result, response_code, _headers, body):
-    if response_code == 200:
-        print("Processing response:", body)
-        var endpoint_data = endpoints.get(current_endpoint)
-        if endpoint_data != null:
-            var calculate_method = endpoint_data.calculate
-            var visualize_method = endpoint_data.visualize
-
-            var data: Dictionary = call(calculate_method, body)
-            call(visualize_method, data)
-        else:
-            print("Endpoint configuration not found for", current_endpoint)
-    else:
-        print("Request failed with response code:", response_code)
 
 func receive_udp_response():
     while udp_client.get_available_packet_count():
@@ -142,7 +102,7 @@ func process_response_udp(response: String):
     # Parse the fragment
     var delimiter_index: int = response.find(":")
     if delimiter_index == -1:
-        print("Received malformed packet: %s" % response)
+        print("Received malformed packet: ", response)
         return
 
     var header: String = response.substr(0, delimiter_index)
@@ -151,7 +111,7 @@ func process_response_udp(response: String):
     # Parse the header (format: "fragment_num/total_fragments")
     var fragment_info: PackedStringArray = header.split("/")
     if fragment_info.size() != 2:
-        print("Received malformed packet header: %s" % header)
+        print("Received malformed packet header: ", header)
         return
 
     var fragment_num: int = int(fragment_info[0])
@@ -168,37 +128,40 @@ func process_response_udp(response: String):
             complete_response += packet_fragments[i]
 
         # All fragments received, process the response
-        _on_request_completed_udp(null, 200, [], complete_response)
+        process_data(complete_response)
 
         # Clear stored fragments for the next message
         packet_fragments.clear()
 
-# Function to handle the completion of the HTTP request
-func _on_request_completed_udp(_result, response_code, _headers, body):
+func _process_response_http(_result, response_code, _headers, body):
     if response_code == 200:
-        var json = JSON.new()
-        var json_result = json.parse(body)
-
-        if json_result == OK:
-            var parsed_data = json.get_data()
-            if parsed_data.size() > 0:
-                # Call the appropriate method for traffic calculation and visualization
-                var endpoint_data = endpoints.get(current_endpoint)
-                if endpoint_data != null:
-                    var calculate_method = endpoint_data.calculate
-                    var visualize_method = endpoint_data.visualize
-
-                    var data: Dictionary = call(calculate_method, parsed_data)
-                    call(visualize_method, data)
-                else:
-                    print("Endpoint configuration not found for", current_endpoint)
-            else:
-                print("No data found in the JSON response")
-        else:
-            print("Failed to parse JSON: ", json.error_string)
+        var response: String = body.get_string_from_utf8()
+        process_data(response)
     else:
-        print("Request failed with response code: ", response_code)
+        print("HTTP request failed with response code: ", response_code)
 
+# Process the data returned by the call to the API
+func process_data(body):
+    var json = JSON.new()
+    var json_result = json.parse(body)
+
+    if json_result == OK:
+        var parsed_data = json.get_data()
+        if parsed_data.size() > 0:
+            # Call the appropriate method for traffic calculation and visualization
+            var endpoint_data = endpoints.get(current_endpoint)
+            if endpoint_data != null:
+                var calculate_method = endpoint_data.calculate
+                var visualize_method = endpoint_data.visualize
+
+                var data: Dictionary = call(calculate_method, parsed_data)
+                call(visualize_method, data)
+            else:
+                print("Endpoint configuration not found for", current_endpoint)
+        else:
+            print("No data found in the JSON response")
+    else:
+        print("Failed to parse JSON: ", json.error_string)
 
 func calculate_traffic_remote(parsed_data: Array) -> Dictionary:
     return nv.calculate_traffic_remote(parsed_data)
@@ -207,9 +170,9 @@ func calculate_traffic_raw(parsed_data: Array) -> Dictionary:
     return nv.calculate_traffic_raw(parsed_data)
 
 func visualize_data_remote(data: Dictionary) -> void:
-    print("Visualizing remote data:", data)
+    print("Visualizing remote data: ", data)
     nv.visualize_data_remote(endpoints.get(current_endpoint), data)
 
 func visualize_data_raw(data: Dictionary) -> void:
-    print("Visualizing raw data:", data)
+    print("Visualizing raw data: ", data)
     nv.visualize_data_raw(endpoints.get(current_endpoint), data)
